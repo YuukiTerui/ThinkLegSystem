3f9aac7fa0cc3fc45c4330343862446fa3c13d49
YuukiTerui <teriyaki0839@gmail.com>
2021-05-19 01:18:13 +0900

    Update stroop.py

--- stroop.py
+++ stroop.py
@@ -1,4 +1,5 @@
 import dataclasses
+from os import linesep
 import tkinter as tk
 from tkinter import ttk
 import csv
@@ -18,7 +19,7 @@ class Stroop(tk.Frame):
         def __call__(self):
             return self.correct, self.choices
 
-    def __init__(self, task, master=None, fname=None, path=r'./'):
+    def __init__(self, task, master=None, fname=None, path=r'./', limit_cnt=None, limit_second=None):
         super().__init__(master)
         self.master = master
         self.master.config(bg='light gray')
@@ -33,13 +34,16 @@ class Stroop(tk.Frame):
         self.fpath = path
         self.fname = fname if fname else datetime.now().isoformat()
         self.cnt = 0
+        self.limit_cnt = limit_cnt
+        self.limit_time = limit_second
+        self.start_time = time.time()
         self.output_data = []
-        self.column = ['correct', 'time', 'choice0', 'choice1', 'choice2', 'choice3', 'choice4', 'answer', 'result', 'answer_time']
+        self.column = ['cnt', 'time', 'correct', 'answer', 'result', 'choice1', 'choice2', 'choice3', 'choice4', 'choice5', 'result']
         self.stream_data = None
 
         self.create_widgets()
         self.pack(anchor=tk.CENTER)
-        self.start_time = time.time()
+        
         self.dt = 0
 
     def create_widgets(self):
@@ -62,16 +66,30 @@ class Stroop(tk.Frame):
             correct, choice = self.stream_data()
             answer = choice[num]
             #result = 1 if correct == answer else 0
-            self.output_data.append([self.cnt, self.t, correct, answer] + choice)
+            self.output_data.append([self.cnt, self.t, correct, answer, int(correct==answer)] + choice)
             
             self.task()
         return process
+    
+    def over_limit(self):
+        t = time.time()
+        if self.limit_cnt and self.limit_time:
+            return self.limit_time <= t - self.start_time or self.limit_cnt <= self.cnt
+        elif self.limit_cnt:
+            return self.limit_cnt <= self.cnt
+        elif self.limit_time:
+            return self.limit_time <= t - self.start_time
+        else:
+            return False
 
     def task1(self):
         '''
         逆ストループ統制課題
         黒インクで書かれた単語が意味する色をその右側の5種の色パッチの中から選ぶ．
         '''
+        if self.over_limit():
+            self.finish()
+
         self.color_label['fg'] = 'black'
         self.color_label['text'] = random.choice(list(self.colors.values()))
         self.color_label.pack(side=tk.LEFT, padx=10, anchor=tk.CENTER, fill='x')
@@ -88,6 +106,8 @@ class Stroop(tk.Frame):
         逆ストループ課題
         色・色名不一致語の単語が意味する色をその右側の色パッチの中から選ぶ．
         '''
+        if self.over_limit():
+            self.finish()
         self.color_label['fg'] = random.choice(list(self.colors.keys()))
         self.color_label['text'] = random.choice(list(self.colors.values()))
         self.color_label.pack(side=tk.LEFT, padx=10, anchor=tk.CENTER)
@@ -104,6 +124,8 @@ class Stroop(tk.Frame):
         ストループ統制課題
         色パッチのインクの色に対する色名語を選ぶ．
         '''
+        if self.over_limit():
+            self.finish()
         self.color_label['bg'] = random.choice(list(self.colors.keys()))
         self.color_label['text'] = ''
         self.color_label.pack(side=tk.LEFT, padx=10, anchor=tk.CENTER)
@@ -122,6 +144,8 @@ class Stroop(tk.Frame):
         ストループ課題
         色・色名不一致語のインクの色に対する色名語を選ぶ
         '''
+        if self.over_limit():
+            self.finish()
         self.color_label['bg'] = 'light gray'
         self.color_label['fg'] = random.choice(list(self.colors.keys()))
         self.color_label['text'] = random.choice(list(self.colors.values()))
@@ -139,9 +163,10 @@ class Stroop(tk.Frame):
     def save(self):
         print(*self.output_data, sep='\n')
         #TODO Decide the directory for writing csv files
-        fname = f'task{self.task_num}.csv'
-        with open(self.fpath+fname, 'w') as f:
+        fname = f'task{self.task_num+1}.csv'
+        with open(self.fpath+fname, 'w', newline='') as f:
             writer = csv.writer(f, delimiter=',')
+            writer.writerow(self.column)
             writer.writerows(self.output_data)
 
     def finish(self):
@@ -161,7 +186,7 @@ def main():
         root.title = "Stroop"
         root.geometry(f"{width}x{height}")
 
-        app = Stroop(task, master=root,fname='stroop_test')
+        app = Stroop(task, master=root,fname='stroop_test', limit_cnt=5, limit_second=10)
         app.mainloop()
 
 
