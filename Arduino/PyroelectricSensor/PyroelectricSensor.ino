4fada5d09d3ca80c7835b3288833da06abf38906
YuukiTerui <teriyaki0839@gmail.com>
2021-05-23 20:54:40 +0900

    Update PyroelectricSensor.ino

--- Arduino/PyroelectricSensor/PyroelectricSensor.ino
+++ Arduino/PyroelectricSensor/PyroelectricSensor.ino
@@ -1,14 +1,19 @@
 int data;
-int INPUTPIN = A0;
-int OUTPUTPIN = 6;
+int INPUT_PIN = A0;
+int OUTPUT_PIN = 6;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
-  data = analogRead(INPUTPIN);
-  Serial.print(data);
-  analogWrite(OUTPUTPIN, data);
-  delay(20);
+  data = analogRead(INPUT_PIN);
+  Serial.println(data);
+  analogWrite(OUTPUT_PIN, data); 
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
 }
