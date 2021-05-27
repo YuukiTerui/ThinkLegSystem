3ca30313f0a73ca85568b4f9a2479a1fd21612fa
YuukiTerui <teriyaki0839@gmail.com>
2021-05-27 22:52:15 +0900

    Update vas.py

--- tasks/vas.py
+++ tasks/vas.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk 
 import csv
 from datetime import datetime
+from typing import Sized
 
 from baseapp import BaseFrame
 
@@ -12,24 +13,26 @@ class VasFrame(BaseFrame):
         self.fpath = path
         self.val = tk.IntVar(self.master, 50)
         self.create_widgets()
-        self.pack(pady=10)
+        self.pack(fill=tk.BOTH, expand=True)
 
     def create_widgets(self):
         font = [("MSゴシック", "15", "bold"), ("MSゴシック", "10", "bold"), ("MSゴシック", "5", "bold")]
         question = "今現在のあなたの疲労感について，適当な場所にマーカーを動かしてください．"
-        self.question_label = tk.Label(self.master, 
+        self.question_label = tk.Label(self, 
             text=question, font=font[0],
-        )
-        self.question_label.pack(pady=50)
 
-        self.scale_frame = tk.Frame(self.master, relief=tk.RAISED)
+        )
+        self.question_label.pack(pady=30, fill=tk.X, expand=True)
+
+        self.scale_frame = tk.Frame(self, relief=tk.RAISED)
         self.scale_label_min = tk.Label(self.scale_frame,
-            text='疲労感はない', font=font[1]
+            text='疲労感はない', font=font[1],
         )
         self.scale_label_min.pack(side=tk.LEFT)
 
         self.scale = tk.Scale(self.scale_frame,
-            variable=self.val, orient=tk.HORIZONTAL, length=500,
+            variable=self.val, orient=tk.HORIZONTAL, length=800,
+
             from_=0, to=100, showvalue=False,
             command=lambda e: print(f"val:{self.val.get():4}")
         )
@@ -39,12 +42,14 @@ class VasFrame(BaseFrame):
             text='想像しうる\n最大の疲労感', font=font[1]
         )
         self.scale_label_max.pack(side=tk.LEFT)
-        self.scale_frame.pack(pady=50)
+
+        self.scale_frame.pack(pady=20, expand=True)
 
         self.submit_button = tk.Button(
-            self, text="Submit", command=self.submit, font=font[1]
+            self, text=" Submit ", command=self.submit, font=font[0],
+            padx=50, pady=30,
         )
-        self.submit_button.pack(anchor=tk.SE, side=tk.RIGHT)
+        self.submit_button.pack(anchor=tk.SE, side=tk.RIGHT, fill=tk.X, padx=30, pady=30)
 
     def submit(self):
         self.save()
