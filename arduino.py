ec6b7e9dbd44d966c0b1ff08d3bab3f993859e6f
YuukiTerui <teriyaki0839@gmail.com>
2021-05-27 14:30:22 +0900

    Update arduino.py

--- arduino.py
+++ arduino.py
@@ -1,15 +1,17 @@
 import os
+import csv
 import time
 import serial
 
 
 class Arduino:
-    def __init__(self, fname='arduino', path='./') -> None:
-        self.datas = None
-        self.start_time = None
-        self.data_cnt = 0
-        self.fname = fname
+    def __init__(self, path='./', fname='arduino') -> None:
         self.path = path
+        self.fname = fname
+        self.data_cnt = 0
+        self.datas = []
+        self.start_time = None
+        self.running = False
         
         self.port = 'COM6' if os.name == 'nt' else '/dev/ttyACM0'
         self.baudrate = 115200
@@ -21,31 +23,60 @@ class Arduino:
 
     @property
     def data(self):
-        data = self.serial.readline().decode('utf-8')
+        return self.datas[-1]
+
+    def __serve(self):
+        data = self.serial.readline()
+        try:
+            data = data.decode('utf-8').replace('\n', '').split(',')
+        except Exception as e:
+            print(data)
+            data = None
         return data
-        
+
     def flush_buffer(self):
         self.serial.reset_output_buffer()
         self.serial.reset_input_buffer()
 
+    def run(self):
+        try:
+            while self.running:
+                data = self.__serve()
+                if data:
+                    self.datas.append(data)
+
+        except:
+            pass
+        else:
+            pass
+        finally:
+            pass
+
     def start(self):
         self.serial.write(b'1')
+        self.running = True
 
     def stop(self):
         self.serial.write(b'0')
+        self.running = False
 
     def reset(self):
         self.serial.write(b'9')
-    
+        self.running = False
+
+    def save(self):
+        with open(f'{self.path}{self.fname}.csv', 'w') as f:
+            weiter = csv.writer(f)
+            weiter.writerows(self.datas)
 
 
 def main():
     arduino = Arduino()
     try:
         while True:
-            data = arduino.get_data()
+            data = arduino.data
             try:
-                print(data.decode('utf-8'))
+                data
             except Exception as e:
                 print(e)
                 print(data)
@@ -54,8 +85,7 @@ def main():
     except KeyboardInterrupt as e:
         print('finish with Cntl-C')
         arduino.serial.close()
-
-
+    
 
 
 
