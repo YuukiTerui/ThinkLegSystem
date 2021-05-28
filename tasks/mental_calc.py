038dc5c6e25071a79c8eba9f6a7a90364723cd36
YuukiTerui <teriyaki0839@gmail.com>
2021-05-28 20:45:01 +0900

    add: task function

--- tasks/mental_calc.py
+++ tasks/mental_calc.py
@@ -1,7 +1,11 @@
 # coding: utf-8
 
 import os
+import random
 import time
+import schedule
+import tkinter as tk
+from random import choices
 from mpyg321.mpyg321 import MPyg321Player
 
 from baseapp import BaseFrame
@@ -9,9 +13,11 @@ from baseapp import BaseFrame
 
 class MentalCalc(BaseFrame):
     def __init__(self):
+        super().__init__()
         self.sounds_path = './sounds/'
         self.sounds = sorted(os.listdir(self.sounds_path))
         self.player = MPyg321Player()
+        self.datas = []
 
     def sound_test(self):
         for i in range(10):
@@ -22,12 +28,34 @@ class MentalCalc(BaseFrame):
         print(f'play {self.sounds_path}{self.sounds[i]}')
         self.player.play_song(f'{self.sounds_path}{self.sounds[i]}')
 
+    def task(self):
+        nums = choices(list(range(10)), k=4)
+        print(nums)
+        correct_num = sum(nums)
+        question_num = None
+        
+        def question(i):
+            self.play(i)
+            return time.time()
+
+        def answer():
+            return time.time()
+
+        def result():
+            pass
+
+        def process(qnum=len(nums), interval=4):
+            for i in range(qnum):
+                time.sleep(interval)
+                question(nums[i])
+            answer()
+
+        process()
 
 
 def main():
-    task = MentalCalc()
-    task.sound_test()
-
+    app = MentalCalc()
+    app.mainloop()
 
 if __name__ == '__main__':
     main()
