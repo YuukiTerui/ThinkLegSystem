db7fa3b13c2e9f847ff2360366ec9271273c16d4
YuukiTerui <teriyaki0839@gmail.com>
2021-06-04 16:26:38 +0900

    Update mysocket.py

--- mysocket.py
+++ mysocket.py
@@ -3,12 +3,21 @@
 import socket
 
 class MySocket():
-    def __init__(self, host, port) -> None:
+    def __init__(self, host, port, kind) -> None:
         self.host = host
         self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if kind == 'server':
+            self.init_server()
+        else:
+            self.init_client()
+            
+    def init_server(self, host, port):
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+        self.socket.bind((host, port))
+        self.socket.listen(10)
 
-    def connect(self, host, port):
+    def init_client(self, host, port):
         self.socket.connect((host, port))
 
     def send(self, msg):
@@ -30,3 +39,14 @@ class MySocket():
             bytes_recd += len(chunk)
         return b''.join(chunks)
         
+
+def main():
+    host = '192.168.0.123'
+    port = '12345'
+
+    server = MySocket()
+
+
+
+if __name__ == '__main__':
+    main()
