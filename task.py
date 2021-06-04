d11dce4ec16c387e42cdc98857a1373202b8d13b
YuukiTerui <teriyaki0839@gmail.com>
2021-06-04 18:15:47 +0900

    Update task.py

--- task.py
+++ task.py
@@ -1,21 +1,27 @@
 import tkinter as tk
 from tasks.baseapp import BaseApp, BaseFrame
-
+from tasks.vas import VasFrame
 
 
 class MainApp(BaseApp):
     def __init__(self):
         super().__init__()
         self.state = 0
-        
+        self.frame = None
         self.create_widgets()
 
     def create_widgets(self):
-        self.first_frame = BaseFrame(self)
-        self.first_frame.pack()
+        self.frame = BaseFrame(self)
+        self.frame.pack()
 
-        self.title_label = tk.Label(self.first_frame, text='Think Leg System')
+        self.title_label = tk.Label(self.frame, text='Think Leg System')
         self.title_label.pack()
+
+        self.vas_button = tk.Button(self.frame, text='vas', command=self.create_vas)
+        self.vas_button.pack()
+
+    def create_vas(self):
+        pass
 
 
 
