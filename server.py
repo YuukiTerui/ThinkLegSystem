7097496b47cccfb5da7da65793398e74b4eecc7c
YuukiTerui <teriyai0839@gmail.com>
2021-06-08 17:04:36 +0900

    Update server.py

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from sys import winver
 import time
 import socket
 import threading
+from collections import deque
 import json
 from logging import config, getLogger
 with open('./config/log_conf.json', 'r') as f:
@@ -19,6 +20,20 @@ class ThinkLegServer:
         self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.is_running = False
+        self.data_queue = deque()
+        
+    @property
+    def data(self):
+        try:
+            data = self.data_queue.popleft()
+            self.logger.debug('data is poped.')
+            return data
+        except IndexError as e:
+            self.logger.debug('server_queue is empty.')
+    
+    def clear_data(self):
+        self.logger.info('data is cleard.')
+        self.data_queue.clear()
 
     def run(self):
         self.is_running = True
@@ -28,7 +43,7 @@ class ThinkLegServer:
         while self.is_running:
             self.logger.info('Waiting for connection.')
             client_socket, address = self.socket.accept()
-            client_socket.settimeout(5)
+            #client_socket.settimeout(5)
             self.logger.info('Established connection.')
             t = threading.Thread(target=self.connect_client, args=(client_socket, address))
             t.setDaemon(True)
@@ -38,9 +53,10 @@ class ThinkLegServer:
     def connect_client(self, client, client_address):
         while True:
             try:
-                msg = client.recv(self.BUFFERSIZE)
+                msg = client.recv(self.BUFFERSIZE).decode('utf-8')
                 self.logger.info('receive msg:%s from %s', msg, client_address)
                 client.send(';'.encode('utf-8'))
+                self.data_queue.append(msg)
                 time.sleep(1)
             except socket.error as e:
                 self.logger.warning('socket error:%s', e)
