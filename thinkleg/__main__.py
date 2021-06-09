334ecbe80728a967d83747679a59743099350992
YuukiTerui <teriyaki0839@gmail.com>
2021-06-09 16:27:05 +0900

    Update __main__.py

--- thinkleg/__main__.py
+++ thinkleg/__main__.py
@@ -1,9 +1,13 @@
 import os
+import time
 import threading
 from datetime import datetime
 import tkinter as tk
+from tkinter import ttk
 from json import load
 from logging import config, getLogger
+from tkinter.constants import HORIZONTAL
+from typing import SupportsRound
 with open('./config/log_conf.json', 'r') as f:
     config.dictConfig(load(f))
 
@@ -27,21 +31,13 @@ class ThinkLegApp(BaseApp):
 
         self.frame = None
         self.arduino = Arduino(self.datapath, 'arduino_data.csv')
+
         self.create_widgets()
         self.logger.debug('ThinkLegApp is initialized.')
 
     def create_widgets(self):
-        self.first_frame = BaseFrame(self)
-        self.first_frame.grid(row=0, column=0, sticky="nsew")
-
-        self.title_label = tk.Label(self.first_frame, text='Think Leg System')
-        self.title_label.pack()
-
-        self.vas_button = tk.Button(self.first_frame, text='vas', command=lambda:self.change_frame('vas'))
-        self.vas_button.pack()
-
-        self.calc_button = tk.Button(self.first_frame, text='calc', command=lambda:self.change_frame('calc'))
-        self.calc_button.pack()
+        self.first_frame = MainFrame(self)
+        self.first_frame.grid(row=0, column=0, sticky="nsew")        
         self.logger.debug('widgets are created.')
 
     def create_vas(self):
@@ -64,8 +60,57 @@ class ThinkLegApp(BaseApp):
             self.create_vas()
         elif to == 'calc':
             self.create_calc()
-            
 
+
+class MainFrame(BaseFrame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.create_widgets()
+        if self.master.arduino:
+            threading.Thread(target=self.__progress).start()
+
+    def create_widgets(self):
+        self.title_label = tk.Label(self, text='Think Leg System')
+        self.title_label.pack()
+
+        self.vas_button = tk.Button(self, text='vas', command=lambda:self.change_frame('vas'))
+        self.vas_button.pack()
+
+        self.calc_button = tk.Button(self, text='calc', command=lambda:self.change_frame('calc'))
+        self.calc_button.pack()
+
+        self.progress_frame = tk.Frame(self)
+        self.progress_label = tk.Label(self.progress_frame, text='Preparing for Arduino')
+        self.progress_label.pack()
+        self.progress_var = tk.IntVar(value=0)
+        self.progress_bar = ttk.Progressbar(self.progress_frame,
+            orient=HORIZONTAL, variable=self.progress_var, maximum=60, length=200, mode='determinate'
+        )
+        self.progress_bar.pack()
+        self.progress_frame.pack()
+
+    def __progress(self):
+        st = time.time()
+        latency = 60
+        t = 0
+        while t < latency:
+            t = time.time()-st
+            self.progress_var.set(t)
+        self.progress_label['text'] = 'Arduino Ready.'
+        self.progress_bar.destroy()
+
+
+    def change_frame(self, to):
+        #self.first_frame.pack_forget()
+        self.logger.debug('change_frame is called.')
+        if self.frame:
+            self.logger.debug('%s is destroied.', self.frame)
+            self.frame.destroy()
+        if to == 'vas':
+            self.create_vas()
+        elif to == 'calc':
+            self.create_calc()
 
 
 
