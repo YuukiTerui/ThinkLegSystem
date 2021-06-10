7ee22ea1bf5d1a9c0df0e79962403a821c7b6950
YuukiTerui <teriyai0839@gmail.com>
2021-06-10 17:12:19 +0900

    fix bug

---
+++ Arduino/PyroelectricSensor/PyroelectricSensorrNano.ino
@@ -0,0 +1,56 @@
+
+#define INPUT_PIN A0
+#define LED_PIN 13
+unsigned long time_ = 0;
+unsigned long start_time = 0;
+int data = 0;
+bool send_flag = true;
+
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  Serial.begin(115200);
+  //while(Serial.available()) Serial.read();
+  Serial.print("arduino is avairable\n");
+}
+
+void send_data() {
+  time_ = millis() - start_time;
+  data = analogRead(INPUT_PIN);
+  String s = String(time_);
+  s += ",";
+  s += String(data);
+  s += '\n';
+  Serial.print(s);
+}
+
+void(* resetFunc) (void) = 0;
+
+void loop() {
+  serialEvent();
+  if (send_flag) {
+    send_data();
+  }
+  delay(20);
+}
+void serialEvent() {
+  if(Serial.available() > 0) { // 内部でloop毎にSerial.available()>0の時呼ばれる関数なはずだから要らないのかもしれない．
+    char c = Serial.read();
+    switch (c) {
+      case byte('0'):
+        send_flag = false;
+        digitalWrite(LED_PIN, LOW);
+        break;
+      case byte('1'):
+        send_flag = true;
+        start_time = millis();
+        digitalWrite(LED_PIN, HIGH);
+        break;
+      // default:
+      case byte('9'):
+        resetFunc();
+        break;
+    }
+  }
+}
