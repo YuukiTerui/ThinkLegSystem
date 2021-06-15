6ebf96b164c915322337fae5047353d7b7f4a75b
YuukiTerui <teriyaki0839@gmail.com>
2021-06-15 15:32:46 +0900

    Update mentalcalc.py

--- tasks/frames/mentalcalc.py
+++ tasks/frames/mentalcalc.py
@@ -1,5 +1,8 @@
 import tkinter as tk
+import time
 from json import load
+from tkinter.constants import HIDDEN
+from numpy.random import randint
 from logging import config, getLogger
 with open('./config/log_conf.json', 'r') as f:
     config.dictConfig(load(f))
@@ -8,24 +11,96 @@ from  ..frames.baseframe import BaseFrame
 
 
 class MentalCalcFrame(BaseFrame):
-    def __init__(self, digit, master=None, path='./data/MentalCalc/', fname='mentalcalc.csv'):
+    def __init__(self, digit=2, master=None, path='./data/MentalCalc/', fname='mentalcalc.csv'):
         super().__init__(master)
-        self.logget = None
+        self.logger = getLogger('gui.frame')
         self.path = path
         self.fname = fname
+        self.is_running = False
+        
+        self.question = self.create_question(digit) # [num1, num2]
+        self.questions = [['num1', 'num2']]
+        self.records = [['num1', 'num2', 'answer', 'result', 'time']]
+        self.q_digit = digit
         self.q_cnt = 0
-        self.q_num_max = 10000
-        self.questions = [['num_l1', '+or-', 'num_l2', '=', 'val', 'correct']]
-        self.answers = [['answer', 'result', 'time']]
+        self.q_pos = 0
+        self.q_value = tk.StringVar(value=str(self.question[0]))
 
         self.create_widgets()
+        self.start_time = time.time()
 
     def create_widgets(self):
-        self.q_label = tk.Label(self, text='Ready?', relief=tk.RAISED, bg=self.bg)
+        self.q_label = tk.Label(self, textvariable=self.q_value, relief=tk.RAISED, bg=self.bg)
         self.q_label['font'] = ('MSゴシック', 80, 'bold')
-        self.q_label.bind('<Button-1>', self.mouse_clicked)
-        self.q_label.bind('<Button-3>', self.mouse_clicked)
+        #self.q_label.bind('<Button-1>', self.mouse_clicked)
+        #self.q_label.bind('<Button-3>', self.mouse_clicked)
+        self.q_label.bind('<KeyPress>', self.key_pressed)
+        self.q_label.focus_set()
         self.q_label.pack(expand=True, fill=tk.BOTH)
 
     def mouse_clicked(self, event):
         self.logger.debug("mouse clicked. %s", event)
+        self.q_label.focus_set()
+    
+    def key_pressed(self, event):
+        key_name = event.keysym
+        var = self.q_value.get()
+        POSNUM = 3
+        if self.q_pos in [0, 1]:
+            if key_name == 'Return':
+                self.q_pos = (self.q_pos + 1) % POSNUM
+                self.change_label()
+        else: # self.q_pos == 2
+            if key_name == 'Return':
+                if not var == 0:
+                    self.submit_answer() # TODO create function
+                    self.question = self.create_question(self.q_digit)
+                    self.q_pos = (self.q_pos + 1) % POSNUM
+                    self.change_label()
+            elif key_name == 'BackSpace':
+                if len(var) == 1:
+                    self.q_value.set('0')
+                else:
+                    self.q_value.set(var[:-1])
+            elif key_name.isdigit():
+                if var == '0':
+                    var = ''
+                self.q_value.set(var + key_name)
+            else:
+                print(key_name)
+    
+    def change_label(self):
+        if self.q_pos == 0:
+            self.q_value.set(self.question[0])
+        elif self.q_pos == 1:
+            self.q_value.set(f'+{self.question[1]}')
+        else:
+            self.q_value.set('0')
+    
+    def submit_answer(self):
+        num1, num2 = self.question
+        ans = int(self.q_value.get())
+        result = (num1 + num2 == ans)
+        t = time.time() - self.start_time
+        ls = [num1, num2, ans, result, t]
+        self.records.append(ls)
+        self.logger.info('record: %s', ls)
+
+    @staticmethod
+    def create_question(digit):
+        '''
+        繰り上がりの起きないdigit桁の整数の足し算を作成する．
+        '''
+        low = 10**(digit-1)
+        high = 9*(10**(digit-1))
+        num1 = randint(low=low, high=high)
+        for i, v in enumerate(str(num1)):
+            if i == 0:
+                num2 = str(randint(low=1, high=10-int(v)))
+            else:
+                num2 += str(randint(low=0, high=10-int(v)))
+        else:
+            num2 = int(num2)
+        return [num1, num2]
+
+
