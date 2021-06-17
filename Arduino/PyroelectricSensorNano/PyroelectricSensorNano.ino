2e0212fa8a4859c11d9d2c6e989bbdc1f9c571b1
YuukiTerui <teriyai0839@gmail.com>
2021-06-17 17:27:23 +0900

    Update PyroelectricSensorNano.ino

--- Arduino/PyroelectricSensorNano/PyroelectricSensorNano.ino
+++ Arduino/PyroelectricSensorNano/PyroelectricSensorNano.ino
@@ -1,39 +1,57 @@
-
-#define INPUT_PIN A0
 #define LED_PIN 13
 unsigned long time_ = 0;
 unsigned long start_time = 0;
-int data = 0;
+unsigned long read_time = 0;
+int interval = 20;
+int v = 0;
+int data = 512;
 boolean send_flag = false;
-
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, LOW);
   Serial.begin(115200);
-  //while(Serial.available()) Serial.read();
   Serial.print("arduino is avairable\n");
 }
 
-void send_data() {
+void(* resetFunc) (void) = 0;
+
+void read_v() {
+  v = analogRead(A0);
+}
+
+int LPF(int y0, int raw) {
+  float alpha = 0.7;
+  float y;
+  y = alpha * y0+ (1-alpha) * raw;
+  return int(y);
+}
+
+void send_to_RPi() {
   time_ = millis() - start_time;
-  data = analogRead(INPUT_PIN);
+  data = LPF(data, v);
   String s = String(time_);
+  s += ",";
+  s += String(raw);
   s += ",";
   s += String(data);
   s += '\n';
   Serial.print(s);
 }
 
-void(* resetFunc) (void) = 0;
-
 void loop() {
   serialEvent();
-  if (send_flag) {
-    send_data();
+  read_v();
+  int tmp_time = millis()-time_read;
+  if (tmp_time >= interval) {
+    time_ = tmp_time;
+    if (send_flag) {
+      send_data();
+    }
+    time_read = millis();
   }
-  delay(20);
 }
+
 void serialEvent() {
   if(Serial.available() > 0) { // 内部でloop毎にSerial.available()>0の時呼ばれる関数なはずだから要らないのかもしれない．
     char c = Serial.read();
