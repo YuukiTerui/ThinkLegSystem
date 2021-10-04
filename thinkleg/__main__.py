8177ed3153c6ac8035bd6d60c68489414310f2a0
YuukiTerui <teriyai0839@gmail.com>
2021-10-04 16:47:22 +0900

    Update __main__.py

--- thinkleg/__main__.py
+++ thinkleg/__main__.py
@@ -47,8 +47,9 @@ class ThinkLegApp(BaseApp):
     def set_frame(self, to, timelimit=None, fname=None):
         self.logger.debug('set_frame is called.')
         self.arduino.thinkleg_status = to
-        if not '.csv' in fname:
-            fname += '.csv'
+        if fname is not None:
+            if not '.csv' in fname:
+                fname += '.csv'
         if to == 'vas':
             self.frame = VasFrame(self, path=self.datapath, fname='vas.csv')
         elif 'mentalcalc' in to:
